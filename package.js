95f76325e9b4f63ce52ebc4de1c9a4fad4dcc796
Nicolás López <nicolaslopezj@me.com>
2014-12-18 13:19:13 -0300

    initial

---
+++ package.js
@@ -0,0 +1,20 @@
+Package.describe({
+	name: 'orionjs:styles',
+	summary: 'Styles for orionjs admin',
+	version: '0.0.1',
+	git: 'https://github.com/orionjs/styles'
+});
+
+Package.onUse(function(api) {
+	api.versionsFrom('1.0');
+
+	api.use('less');
+
+	api.addFiles([
+		'lib/global.less',
+		], 'client');
+});
+
+Package.onTest(function(api) {
+	api.use('tinytest');
+});
